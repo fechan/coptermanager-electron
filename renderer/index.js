ed6349165dc683028bc512572597de663088a569
fechan <56131910+fechan@users.noreply.github.com>
2021-12-27 15:42:54 -0800

    allow gamepad to change throttle

--- renderer/index.js
+++ renderer/index.js
@@ -1,15 +1,32 @@
 // window.copter.bindCopter();
+let bound = false;
+let gamepadPollInterval = setInterval(pollGamepads, 500);
 
-// setInterval(() => {
-//   const gamepad = window.navigator.getGamepads()[0];
-//   window.copter.changeThrottle(gamepad.axes[1] * 0x08);
-//   console.log(`Left stick at (${gamepad.axes[0]}, ${gamepad.axes[1]})` );
-//   console.log(`Right stick at (${gamepad.axes[2]}, ${gamepad.axes[3]})` );
-// }, 100);
+function pollGamepads() {
+  let gamepads = navigator.getGamepads();
+  for (let i = 0; i < gamepads.length; i++) {
+    let gamepad = gamepads[i];
+    if (gamepad) {
+      document.getElementById("log").scrollTop = document.getElementById("log").scrollHeight 
+      document.getElementById("log").innerHTML += `Gamepad connected at index ${gamepad.index}: ${gamepad.id}. It has ${gamepad.buttons.length} buttons and ${gamepad.axes.length} axes.\n`;
+      clearInterval(gamepadPollInterval);
+      controllerLoop();
+    }
+  }
+}
 
-document.getElementById("bind").onclick = () => window.copter.bindCopter();
+function controllerLoop() {
+  let gamepads = navigator.getGamepads();
+  let gamepad = gamepads[0];
+  if (bound) {
+    window.copter.changeThrottle((-gamepad.axes[1] * 0x0F) | 0);
+  }
+  requestAnimationFrame(controllerLoop);
+}
 
-document.getElementById("disconnect").onclick = () => window.copter.disconnectCopter();
+document.getElementById("bind").onclick = () => {window.copter.bindCopter();};
+
+document.getElementById("disconnect").onclick = () => {window.copter.disconnectCopter(); bound = false};
 
 document.getElementById("throttle-up").onclick = () => window.copter.changeThrottle(0x0F);
 
@@ -18,4 +35,7 @@ document.getElementById("throttle-down").onclick = () => window.copter.changeThr
 
 window.copter.receive("log", (event, data) => {
   document.getElementById("log").innerHTML += data + "\n";
+  if (data.includes("Received data 0001")) {
+    bound = true;
+  }
 });
